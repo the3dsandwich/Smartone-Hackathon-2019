4444b045fcf693703164ab58cbb3556bc9b1bb8c
Shann Wei Yeh <the3dsandwich@the3dsandbook.local>
2019-10-27 01:05:22 +0800

    button for toggling AddForm

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -5,70 +5,62 @@ export const AddForm = () => {
   switch (formState) {
     case "select-category":
       return (
-        <form onSubmit="">
-          <label>
-            select category
-          </label>
+        <form onSubmit={null}>
+          <label>select category</label>
           <select>
-             <option value = "discount"> Discount </option>
-             <option value = "event"> Event </option>
-             <option value = "incident"> Incident </option>
+            <option value="discount"> Discount </option>
+            <option value="event"> Event </option>
+            <option value="incident"> Incident </option>
           </select>
           <br></br>
-          <input type = "submit" value = "Next"></input>
+          <input type="submit" value="Next"></input>
         </form>
       );
     case "select-subtype":
       return (
-        <form onSubmit="">
-          <label>
-            select discount type
-          </label>
-          <select>
-             <option value = "discount"> Foods and Beverages </option>
-             <option value = "event"> Stores </option>
-             <option value = "incident"> Attractions </option>
-             <option value = "incident"> Hotels </option>
-          </select>
-          <br></br>
-          <input type = "submit" value = "Next"></input>
-        </form>
-        <form onSubmit="">
-        <label>
-          select event type
-        </label>
-        <select>
-           <option value = "discount"> Markets </option>
-           <option value = "event"> Music </option>
-           <option value = "incident"> Parade </option>
-           <option value = "incident"> Sports </option>
-        </select>
-        <br></br>
-        <input type = "submit" value = "Next"></input>
-      </form>
-      <form onSubmit="">
-      <label>
-        select incident type
-      </label>
-      <select>
-         <option value = "discount"> Traffic </option>
-         <option value = "event"> Gathering </option>
-         <option value = "incident"> Fire </option>
-         <option value = "incident"> Gas Leakage </option>
-         <option value = "incident"> Air Quality </option>
-      </select>
-      <br></br>
-      <input type = "submit" value = "Next"></input>
-    </form>
+        <div>
+          <form onSubmit={null}>
+            <label>select discount type</label>
+            <select>
+              <option value="discount"> Foods and Beverages </option>
+              <option value="event"> Stores </option>
+              <option value="incident"> Attractions </option>
+              <option value="incident"> Hotels </option>
+            </select>
+            <br></br>
+            <input type="submit" value="Next"></input>
+          </form>
+          <form onSubmit="">
+            <label>select event type</label>
+            <select>
+              <option value="discount"> Markets </option>
+              <option value="event"> Music </option>
+              <option value="incident"> Parade </option>
+              <option value="incident"> Sports </option>
+            </select>
+            <br></br>
+            <input type="submit" value="Next"></input>
+          </form>
+          <form onSubmit="">
+            <label>select incident type</label>
+            <select>
+              <option value="discount"> Traffic </option>
+              <option value="event"> Gathering </option>
+              <option value="incident"> Fire </option>
+              <option value="incident"> Gas Leakage </option>
+              <option value="incident"> Air Quality </option>
+            </select>
+            <br></br>
+            <input type="submit" value="Next"></input>
+          </form>
+        </div>
       );
     case "enter-description":
       return (
         <form>
-          <label>
-            The description: 
-          </label>
-          <input type = "text" name = "description"></input>
-          <input type = "submit" value = "Next"></input>
+          <label>The description:</label>
+          <input type="text" name="description"></input>
+          <input type="submit" value="Next"></input>
         </form>
       );
     case "submit":
